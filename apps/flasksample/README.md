cbe9166ef99c3139cb53907869f45ae436bae775
Robert James Hernandez <rob@sarcasticadmin.com>
2021-06-22 12:32:10 -0700

    Init flasksample app from fork

---
+++ apps/flasksample/README.md
@@ -0,0 +1,5 @@
+# Python Flask App
+
+Application originated from: https://github.com/sjeeva/flaskapp
+
+Has since been adapted to fit the needs of the Nebulaworks team
---
+++ apps/flasksample/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+# Sometimes requirements in setup.py are supposed to be just whats required to run
+# the application. This this case thats teh same as requirements.txt
+# For more info: https://packaging.python.org/discussions/install-requires-vs-requirements/
+requirements = [line.strip() for line in open("requirements.txt").readlines()]
+
+# Version here doesnt matter much since we are not
+# installing this outside of our repo or shipping
+# to pypi
+setup(
+    version='0.1.0',
+    name='flasksample',
+    description='flask sample app',
+    author='NWI Engineering',
+    author_email='engineering@nebulaworks.com',
+    url='https://github.com/Nebulaworks/orion',
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=requirements
+)
