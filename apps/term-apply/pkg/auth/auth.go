e701044b7714d0c8d09cdd1ce4da2a19d3a9c827
kylerisse <kylerisse@users.noreply.github.com>
2022-04-01 14:45:33 -0700

    logging attempted ssh keys

--- apps/term-apply/pkg/auth/auth.go
+++ apps/term-apply/pkg/auth/auth.go
@@ -12,6 +12,9 @@ import (
 )
 
 func compareKeys(username string, key ssh.PublicKey) bool {
+	tryfp := gossh.FingerprintSHA256(key)
+	log.Printf("username: %s attempting to auth with %s", username, tryfp)
+
 	// Should probably cache this since each auth type will invoke the service
 	// reaching out to github
 	r, err := http.Get(fmt.Sprintf("https://github.com/%s.keys", username))
@@ -25,7 +28,6 @@ func compareKeys(username string, key ssh.PublicKey) bool {
 
 	scanner := bufio.NewScanner(r.Body)
 
-	counter := 1
 	for scanner.Scan() {
 		linea := scanner.Text()
 		line, _, _, _, err := gossh.ParseAuthorizedKey([]byte(linea))
@@ -34,11 +36,11 @@ func compareKeys(username string, key ssh.PublicKey) bool {
 			return false
 		}
 		fp := gossh.FingerprintSHA256(line)
-		if fp == gossh.FingerprintSHA256(key) {
-			fmt.Printf("username: %s found match: %s\n", username, fp)
+		if fp == tryfp {
+			log.Printf("username: %s found match: %s", username, fp)
 			return true
 		}
-		counter++
+		log.Printf("username: %s not a match: %s", username, fp)
 	}
 	if scanner.Err() != nil {
 		log.Println(scanner.Err())
