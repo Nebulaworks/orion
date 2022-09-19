1e123f86a7e4d513a3182c0280eb19b64c77a593
Chuck Steinman <chuck@nebulaworks.com>
2022-09-19 13:38:49 -0700

    Use S3 as source of truth for uploaded resumes

---
+++ apps/term-apply/pkg/applicant/resume_test.go
@@ -0,0 +1,31 @@
+package applicant
+
+import "testing"
+
+func swapS3KeyExists(value func(string, string) bool) {
+	checkS3KeyExists = value
+}
+
+func shouldExist(string, string) bool {
+	return true
+}
+
+func shouldNotExist(string, string) bool {
+	return false
+}
+
+func TestIsUploaded(t *testing.T) {
+	defer swapS3KeyExists(checkS3KeyExists)
+
+	watcher, _ := newResumeWatcher("notarealbucket", "fakeprefix")
+
+	swapS3KeyExists(shouldExist)
+	if !watcher.isUploaded("nothing") {
+		t.Fatalf("It should show as uploaded")
+	}
+
+	swapS3KeyExists(shouldNotExist)
+	if !watcher.isUploaded("nothing") {
+		t.Fatalf("It should not show as uploaded")
+	}
+}
